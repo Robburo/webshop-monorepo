750655837c7c8318a96ab48dae63503ea11cd988
Robin <robinburo@gmail.com>
2025-08-18 21:17:13 +0200

    Feat: Enums

---
+++ backend/src/main/java/webshop/backend/domains/order/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package webshop.backend.domains.order.enums;
+
+public enum OrderStatus {
+    PENDING,
+    PAID,
+    SHIPPED,
+    COMPLETED,
+    CANCELED
+}
---
+++ backend/src/main/java/webshop/backend/domains/user/enums/UserRole.java
@@ -0,0 +1,6 @@
+package webshop.backend.domains.user.enums;
+
+public enum UserRole {
+    ROLE_USER,
+    ROLE_ADMIN
+}
