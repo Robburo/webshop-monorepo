9bc9d1a947e9291a6b0030d08f0afb1226a1a747
Robin <robinburo@gmail.com>
2025-08-22 10:52:00 +0200

    Bug: Added some tests for controller layer

---
+++ backend/src/test/java/webshop/backend/domains/category/controller/CategoryControllerTest.java
@@ -0,0 +1,7 @@
+package webshop.backend.domains.category.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryControllerTest {
+
+}
---
+++ backend/src/test/java/webshop/backend/domains/order/controller/OrderControllerTest.java
@@ -0,0 +1,7 @@
+package webshop.backend.domains.order.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderControllerTest {
+
+}
