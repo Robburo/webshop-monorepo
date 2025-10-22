b9f7279aca2dcbe970d1af4f1cf8be7721ce52bf
Robin <robinburo@gmail.com>
2025-10-22 13:47:15 +0200

    BackButton

--- frontend/src/components/BackButton.tsx
+++ frontend/src/components/BackButton.tsx
@@ -1,10 +1,13 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 
 export default function BackButton() {
   const router = useRouter();
+  const pathname = usePathname();
 
+  if (pathname === "/") return null;
+  
   return (
     <button
       onClick={() => router.back()}
